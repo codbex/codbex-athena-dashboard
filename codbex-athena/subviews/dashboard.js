fe48cc0067fb8d8a5216abb6025d8cce924a35fe
MrGoblings <mrgoblings@gmail.com>
2024-05-31 05:28:38 +0000

    chore: Removes commented code

--- codbex-athena/subviews/dashboard.js
+++ codbex-athena/subviews/dashboard.js
@@ -67,19 +67,6 @@ dashboard.controller('DashboardController', ['$scope', '$document', '$http', 'me
             calculateGrossProfit();
         });
 
-    // const orderServiceUrl = "/services/ts/codbex-athena/api/OrderService.ts/orderData";
-    // $http.get(orderServiceUrl)
-    //     .then(function (response) {
-    //         $scope.OrderData = response.data;
-    //         calculateGrossProfit();
-    //     });
-
-    // const productServiceUrl = "/services/ts/codbex-athena/api/ProductService.ts/productData";
-    // $http.get(productServiceUrl)
-    //     .then(function (response) {
-    //         $scope.ProductData = response.data;
-    //     });
-
     async function getProductData() {
         try {
             const response = await $http.get("/services/ts/codbex-athena/api/ProductService.ts/productData");
@@ -94,37 +81,5 @@ dashboard.controller('DashboardController', ['$scope', '$document', '$http', 'me
             $scope.GrossProfit = (($scope.InvoiceData.SalesInvoiceTotal + $scope.OrderData.SalesOrderTotal) - ($scope.InvoiceData.PurchaseInvoiceTotal + $scope.OrderData.PurchaseOrderTotal)).toFixed(2);
         }
     }
-
-    // async function getOrderData() {
-    //     try {
-    //         const response = await $http.get("/services/ts/codbex-athena/api/OrderService.ts/orderData");
-    //         return response.data;
-    //     } catch (error) {
-    //         console.error('Error fetching order data:', error);
-    //     }
-    // }
-
-    // angular.element($document[0]).ready(async function () {
-    //     const orderData = await getOrderData();
-    //     $scope.topSalesOrders = orderData.TopSalesOrders;
-    // });
-
-    // angular.element($document[0]).ready(async function () {
-    //     const orderData = await getOrderData();
-    //     $scope.topPurchaseOrders = orderData.TopPurchaseOrders;
-    // });
-
-    // angular.element($document[0]).ready(async function () {
-    //     const orderData = await getOrderData();
-    //     $scope.$apply(function () {
-    //         $scope.topCustomers = orderData.TopCustomers;
-    //     });
-    // });
-
-    // angular.element($document[0]).ready(async function () {
-    //     const productData = await getProductData();
-    //     $scope.topProducts = productData.TopProducts;
-
-    // });
 
 }]);
