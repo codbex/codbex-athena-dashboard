90051c65b1a21366191326b9e4ee65b0ad0643d6
MrGoblings <mrgoblings@gmail.com>
2024-07-03 18:39:59 +0000

    add: added the tile from athena-tiles

--- codbex-athena/subviews/dashboard.js
+++ codbex-athena/subviews/dashboard.js
@@ -1,4 +1,4 @@
-const dashboard = angular.module('dashboard', ['ideUI', 'ideView']);
+const dashboard = angular.module('dashboard', ['ideUI', 'ideView', "applicationTiles"]);
 
 dashboard.controller('DashboardController', ['$scope', '$document', '$http', 'messageHub', function ($scope, $document, $http, messageHub) {
     $scope.state = {
@@ -8,53 +8,13 @@ dashboard.controller('DashboardController', ['$scope', '$document', '$http', 'me
     };
 
     angular.element($document[0]).ready(async function () {
-        const productData = await getProductData();
-        // Doughnut Chart Data
-        const doughnutData = {
-            labels: ['Active Products', 'Inactive Products'],
-            datasets: [{
-                data: [productData.ActiveProducts, productData.InactiveProducts],
-                backgroundColor: ['#36a2eb', '#ff6384']
-            }]
-        };
 
-        // Doughnut Chart Configuration
-        const doughnutOptions = {
-            responsive: true,
-            maintainAspectRatio: false,
-            legend: {
-                position: 'bottom'
-            },
-            title: {
-                display: true,
-                text: 'Product Status'
-            },
-            animation: {
-                animateScale: true,
-                animateRotate: true
-            }
-        };
-        // Initialize Doughnut Chart
-        const doughnutChartCtx = $document[0].getElementById('doughnutChart').getContext('2d');
-        const doughnutChart = new Chart(doughnutChartCtx, {
-            type: 'doughnut',
-            data: doughnutData,
-            options: doughnutOptions
-        });
         $scope.$apply(function () {
             $scope.state.isBusy = false;
         });
     });
 
     $scope.openPerspective = function (perspective) {
-        // if (perspective === 'sales-orders') {
-        //     messageHub.postMessage('launchpad.switch.perspective', { perspectiveId: 'sales-orders' }, true);
-        // } else if (perspective === 'products') {
-        //     messageHub.postMessage('launchpad.switch.perspective', { perspectiveId: 'products' }, true);
-        // } else if (perspective === 'sales-invoices') {
-        //     messageHub.postMessage('launchpad.switch.perspective', { perspectiveId: 'sales-invoices' }, true);
-        // }
-        ;
         messageHub.postMessage('launchpad.switch.perspective', { perspectiveId: 'sales-invoices' }, true);
     }
 
